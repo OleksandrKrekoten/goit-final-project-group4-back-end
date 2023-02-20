d1edd3dcaf217da2650248e44a614069d09cc95e
sonyaaat <sofiatkach2004@gmail.com>
2023-02-20 11:29:04 +0200

    Implemented statistics endpoints

---
+++ controllers/transactions/incomeMonths.js
@@ -0,0 +1,72 @@
+const { Transactions } = require("../../schema/transactionsMongooseSchema");
+const { mothsResultsSchema } = require("../../schema/joiSchemas");
+
+const incomeMonths = async (req, res) => {
+  const { error } = mothsResultsSchema.validate(req.body);
+  if (error) {
+    res.status(404).json({ message: error.message });
+  }
+  // const { month,year } = req.body;
+  // const{_id:userId}=req.user
+  const userId = "1";
+  const year = 2020;
+  const currentMonth = 5;
+
+  const yearStarts = new Date(`Wed, 01 Jan ${year} 00:00:00 GMT`);
+  const yearEnds = new Date(`Thu, 31 Dec ${year} 00:00:00 GMT`);
+
+  const array = await Transactions.aggregate([
+    {
+      $match: {
+        userId,
+        income: true,
+        dateTransaction: {
+          $gte: yearStarts,
+          $lte: yearEnds,
+        },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          month: {
+            $month: "$dateTransaction",
+          },
+        },
+        total: {
+          $sum: "$sum",
+        },
+      },
+    },
+    {
+      $addFields: {
+        month: "$_id.month",
+      },
+    },
+    {
+      $unset: "_id",
+    },
+    {
+      $sort: {
+        month: 1,
+      },
+    },
+  ]);
+  const result = array.filter(({ month }) => month >= currentMonth);
+  const length = result.length;
+
+  if (length < 6) {
+    for (
+      let index = 0;
+      index < 6 - array.length && result.length < array.length;
+      index++
+    ) {
+      if (array[index]) {
+        result.push(array[index]);
+      }
+    }
+  }
+  result.sort((a, b) => a.month - b.month);
+  res.status(200).json(result);
+};
+module.exports = incomeMonths;
--- routes/api/transactions.js
+++ routes/api/transactions.js
@@ -1,25 +1,36 @@
-const express = require('express')
+const express = require("express");
+const {
+  expensesMonths,
+  incomeMonths,
+  fullStatistics,
+} = require("../../controllers/transactions/index");
+const ctrlWrapper = require("../../middlewares/ctrlWrapper");
 
-const router = express.Router()
+const router = express.Router();
+router.get("/incomeMonths", ctrlWrapper(incomeMonths));
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/expensesMonths", ctrlWrapper(expensesMonths));
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/fullStatistics",ctrlWrapper(fullStatistics))
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
 
-module.exports = router
+router.delete("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+router.put("/:contactId", async (req, res, next) => {
+  res.json({ message: "template message" });
+});
+
+module.exports = router;
