484f3106b67eae847ec1bd217969352bc8215343
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2023-02-23 09:48:57 +0200

    user checking is added

--- controllers/auth/user.controller.js
+++ controllers/auth/user.controller.js
@@ -1,6 +1,10 @@
+const createError = require("http-errors");
 const { updateBalance } = require("../../services/userService");
 
 const updateUserBalance = async (req, res) => {
+  if (!req.user) return next(createError(404, "No users found"));
+  if (!req.user.token) return next(createError(401, "Not authorized"));
+
   const { _id, token } = req.user;
   const { balance } = req.body;
 
