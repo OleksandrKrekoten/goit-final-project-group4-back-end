32cbb28b822934651a21838ac50ea2b88eae1615
Oleksandr <alexkrekoten11@gmail.com>
2023-02-23 10:09:56 +0200

    add swagger docs

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 
 const transactionsRouter = require("./routes/api/transactions");
@@ -16,6 +18,7 @@ app.use(express.json());
 
 app.use("/api/transaction", transactionsRouter);
 app.use("/api/auth", authRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   return res.status(404).json({ message: "Not found" });
