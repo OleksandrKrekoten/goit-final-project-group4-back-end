416a2301a7aa1533cea69bafe8556d801f0223ab
Taras Maltsev <98047270+TarasIT@users.noreply.github.com>
2023-03-02 10:36:55 +0200

    google authorization is fixed

--- schema/usersMongooseSchema.js
+++ schema/usersMongooseSchema.js
@@ -8,7 +8,6 @@ const schema = mongoose.Schema(
       lowercase: true,
       unique: true,
       match: [
-        // eslint-disable-next-line no-useless-escape
         /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
         "Please fill a valid email address",
       ],
@@ -21,7 +20,6 @@ const schema = mongoose.Schema(
     balance: {
       type: String,
       default: null,
-      //   required: [true, "Balance is required"],
     },
     verify: {
       type: Boolean,
@@ -29,10 +27,10 @@ const schema = mongoose.Schema(
     },
     verificationToken: {
       type: String,
-      required: [true, "Verify token is required"],
     },
     token: String,
   },
+
   { versionKey: false, timestamps: true }
 );
 const User = mongoose.model("users", schema);
